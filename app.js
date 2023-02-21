9bb16b6b6aa1b8a9d6cbe1dd32aad5272a071f01
Jean Bosco Mugiraneza <mjeanbosco19@gmail.com>
2023-02-21 21:44:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ if (process.env.NODE_ENV === 'development') {
 app.use(cors());
 
 app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/blogs', blogRouter);
@@ -48,7 +50,6 @@ app.use((req, res, next) => {
 
 // ROUTES
 // app.use('/', viewRouter);
-
 
 // app.use('/api/v1/contacts', contactRouter);
 
